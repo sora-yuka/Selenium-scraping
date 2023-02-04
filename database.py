1e5aabcd1eacc1fa91b2ef5e0d22ab630b6c2bd0
sora-yuka <feelingjeez@gmail.com>
2023-02-04 23:00:16 +0600

    comleted

--- database.py
+++ database.py
@@ -1,46 +1,79 @@
-from sqlalchemy import ForeignKey, ForeignKey, ForeignKey, create_engine, Column, String, Integer
+from sqlalchemy import ForeignKey, ForeignKey, ForeignKey, create_engine, Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
-class ScrapData(Base):
-    """Форма создания таблицы в базе данных."""
-    __tablename__ = "title"
+class EnnouncementsData(Base):
+    """ Форма создания таблицы в базе данных. """
+    __tablename__ = "ennouncements"
     
     id = Column("id", Integer, primary_key=True)
-    title = Column("title", String)
+    ennouncement = Column("ennouncement", String)
+    created_time = Column(DateTime(timezone=True), server_default=func.now())
     
-    def __init__(self, title):
-        self.title = title
+    def __init__(self, ennouncement):
+        self.ennouncement = ennouncement
         
     def __repr__(self):
-        return f"{self.title}"
+        return f"{self.ennouncement}"
     
-    
-class AdditionalData(Base):
-    """Форма для создания таблицы в базе данных."""
-    __tablename__ = "chapter"
+
+class EnnouncementPrices(Base):
+    """ Форма для создания таблицы в базе данных. """
+    __tablename__ = "prices"
     
     id = Column("id", Integer, primary_key=True)
-    chapter = Column("chapter", String)
-    title = Column(Integer, ForeignKey("title.id"))
+    price = Column("price", String)
+    created_time = Column(DateTime(timezone=True), server_default=func.now())
     
-    def __init__(self, chapter, title):
-        self.chapter = chapter
-        self.title = title
+    def __init__(self, price):
+        self.price = price
+        
+    def __repr__(self):
+        return f"{self.price}"
+
+
+class EnnouncementPosts(Base):
+    """ Форма для создания таблицы в базе данных. """
+    __tablename__ = "posts"
+    
+    id = Column("id", Integer, primary_key=True)
+    date_posted = Column("post", String)
+    created_time = Column(DateTime(timezone=True), server_default=func.now())
+
+    def __init__(self, date_posted):
+        self.date_posted = date_posted
     
     def __repr__(self):
-        return f"{self.chapter}"
+        return f"{self.date_posted}"
+    
 
+class EnnouncementPictures(Base):
+    """ Форма для создания таблицы в базе данных. """
+    __tablename__ = "pictures"
+    
+    id = Column("id", Integer, primary_key=True)
+    picture = Column("picture", String)
+    created_time = Column(DateTime(timezone=True), server_default=func.now())
+
+    def __init__(self, picture):
+        self.picture = picture
+    
+    def __repr__(self):
+        return f"{self.picture}"
+    
+    
 engine = create_engine("sqlite:///scrap_data.db", echo=True)
 Base.metadata.create_all(bind=engine)
 """^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"""
-"""Подключение к локальной базе данных и создание таблицю"""
+""" Подключение к локальной базе данных и создание таблицю. """
 
 
-"""Если вы пользуетесь существующей базой данных, то подключите ее следующим обрахом."""
+""" Если вы пользуетесь существующей базой данных, то подключите ее следующим обрахом. """
 # engine = create_engine("postgresql+psycopg2://user:password@localhost/database", echo=True)
 
 Session = sessionmaker(bind=engine)
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from database import session, ScrapData, AdditionalData
+from database import (
+    session, EnnouncementsData, EnnouncementPosts, 
+    EnnouncementPrices, EnnouncementPictures
+)
+from datetime import datetime
 import aiohttp
 import asyncio
 
@@ -10,37 +14,51 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 def get_source_code(URI) -> None:
-    """Функция для скрапинга данных с сайтов."""
+    """ Функция для скрапинга данных с сайтов. """
     driver.get(URI)
     
-    titles = driver.find_elements(By.TAG_NAME, "h4")
-    chapters = driver.find_elements(By.XPATH, "//div[contains(@class, 'updates__item')]//div[contains(@class, 'updates__body')]")
-#*  ^^^^^^^^^^^^^^^^^^^^^^^
-    """Получение данных."""
+    """ Получение данных. """
+    ennouncements = driver.find_elements(By.XPATH, "//div[contains(@class, 'container-results large-images')]")
+    ennouncement_posts = driver.find_elements(By.XPATH, "//div[contains(@class, 'location')]")
+    ennouncement_prices = driver.find_elements(By.XPATH, "//div[contains(@class, 'price')]")
+    ennouncement_pictures = driver.find_elements(By.TAG_NAME, "img")
     
-    for title in titles:
-        result = title.text
-        title = ScrapData(result)
-        session.add(title)
-        session.commit()
+    """ Запись полученных данных в БД. """
+    def save_ennouncement(ennouncements):
+        for ennouncement in ennouncements:
+            session.add(EnnouncementsData(ennouncement.text))
+            session.commit()
+    save_ennouncement(ennouncements)
     
-    for chapter in chapters:
-        result = chapter.text
-        chapter = AdditionalData(result, title.id)
-        session.add(chapter)
-        session.commit()
-    """Запись полученных данных в БД."""
+    def save_ennouncement_price(ennouncement_prices):
+        for ennouncement_price in ennouncement_prices:
+            session.add(EnnouncementPrices(ennouncement_price.text))
+            session.commit()
+    save_ennouncement_price(ennouncement_prices)
+
+    def save_ennouncement_post(ennouncement_posts):
+        for ennouncement_post in ennouncement_posts:
+            session.add(EnnouncementPosts(ennouncement_post.text))
+            session.commit()
+    save_ennouncement_post(ennouncement_posts)
+    
+    def save_ennouncement_picture(ennouncement_pictures):
+        for ennouncement_picture in ennouncement_pictures:
+            picture = ennouncement_picture.get_attribute("src")
+            session.add(EnnouncementPictures(picture))
+            session.commit()
+    save_ennouncement_picture(ennouncement_pictures)
     
     
 async def get_html_page(URI) -> None:
-    """Функция отправляющая запрос на сайт и выводящий html код в терминале."""
+    """ Асинхронная функция отправляющая запрос на сайт и выводящий html код в терминале. """
     async with aiohttp.ClientSession() as session:
         async with session.get(URI) as response:
             print(await response.text())
-            
+
 
 def main():
-    BASE_URL = "https://ranobelib.me/?section=home-updates"
+    BASE_URL = "https://www.kijiji.ca/b-apartments-condos/city-of-toronto/c37l1700273"
     get_source_code(BASE_URL)
     asyncio.run(get_html_page(BASE_URL))
     
